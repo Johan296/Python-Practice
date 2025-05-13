8b26600faf83f8d7583c34882530c7cc5c25305b
Johan296 <159972047+Johan296@users.noreply.github.com>
2025-05-13 20:08:50 +1200

    Milestone1

---
+++ milestone_1/milestone_1_code.py
@@ -0,0 +1,14 @@
+name = input("Hello, what is your name ?")
+year = int(input("What year were you born in? ")) 
+current_year = 2025
+age = current_year - year  
+print("Nice to meet you {}, you are {} years old.".format(name, age))
+favorite_color = input("What's your favorite color? ")
+print("{} is a great color!".format(favorite_color))
+hobby = input("What is your favorite hobby? ")
+print("{} sounds really fun!".format(hobby))
+print("It was fun talking to you {}, have a great day!".format(name))
+
+
+
+

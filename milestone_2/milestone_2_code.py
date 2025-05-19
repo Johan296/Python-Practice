e48a364171fcddac5f101033b177d8409ff6b836
Johan296 <159972047+Johan296@users.noreply.github.com>
2025-05-19 20:39:15 +1200

    more chnages

--- milestone_2/milestone_2_code.py
+++ milestone_2/milestone_2_code.py
@@ -8,18 +8,18 @@ age = int(input("How old are you?"))
 if age < 18:
   print("You are a minor and i'm gonna touch you jk.")
 elif age >= 18:
- print("Don't touch me pls!")
+ print("I hope you don't touch me.")
 
 mood = input("How are you feeling today? ")
 if mood.lower() == "happy":
   print("That's awesome! Did you win the lottery or just have coffee?")
 elif mood.lower() == "sad":
-  print("I'm sending you a virtual puppy gif. Hang in there!")
+  print("I'm sending you a puppy gif. Hang in there!")
 
-number = int(input("Pick a number: "))
+number = int(input("Pick a number:"))
 if number <= 10:
   print("That's a tiny number! Just like, someone I know who's tiny aswell.")
 elif 10 < number <= 80:
-  print("That's a medium-sized number. Respectable!")
+  print("That's a medium-sized number.")
 else:
   print("Whoa, that's a huge number!")

53e5741e917a74649016dc49e7c9c4e8210c80a7
Johan296 <159972047+Johan296@users.noreply.github.com>
2025-05-28 19:36:55 +1200

    milestone 3 is sick

--- milestone_2/milestone_2_code.py
+++ milestone_2/milestone_2_code.py
@@ -8,18 +8,18 @@ age = int(input("How old are you?"))
 if age < 18:
   print("Get off your laptop and go play outside.")
 elif age >= 18:
- print("Hello there, unc.")
+ print("Hello there, unc/aunt.")
 
 mood = input("How are you feeling today? (Happy or Sad)").lower()
 if mood == "happy":
     happy_reason = input("That's awesome! Type 'lottery' if you won the lottery or 'coffee' if you just had coffee: ")
     if happy_reason == "lottery":
-     print("Wow, you're rich! Can I borrow some money? Just kidding!")
+            print("Wow, you're rich! Can I borrow some money? Just kidding!")
     elif happy_reason == "coffee":
-        print("Don't drink too much coffee or you will be hyperactive!")
+            print("Don't drink too much coffee or you will be hyperactive!")
     else:
-        print("Whatever the reason, keep smiling!")
+     print("Whatever the reason, keep smiling!")
+    
 elif mood == "sad":
     print("I'm sending you a puppy gif. Hang in there!")
     print("Here's your puppy gif: https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmhuYmd2YWJ2MnpuZWtzMmY0Mnp2NWJ3cXk3bzU2cHJpOHFuZG91MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/cYZkY9HeKgofpQnOUl/giphy.gif")
-    
---
+++ milestone_3/milestone_3_code.py
@@ -0,0 +1,56 @@
+#Music Quiz 
+# This is a simple music quiz coding that asks the user five questions about music.
+score = 0
+print("Welcome to the Music Quiz!")
+
+# Question 1
+answer = input("What is the name of the song that dissed Drake in 2024?")
+if answer== "Not Like Us".lower():
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 2
+answer = input("Who Found the XO record label?")
+if answer == "Weeknd".lower():
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 3
+answer = input("Which Artist released the album ' I AM MUSIC'?")
+if answer == "Playboi Carti".lower():
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 4
+answer = input("Who sang the song 'Juno'?")
+if answer == "Sabrina carpenter".lower():
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 5
+answer = input("What is the real name of rapper Eminem?")
+if answer == "Marshall Mathers".lower():
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 6
+answer = int(input("How many songs does The Weeknd have that are over 1 billion streams ?")) 
+if answer == 27:
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Final score
+print("Quiz completed!")
+print("Score:", score)

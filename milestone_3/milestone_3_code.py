1ac29825c6b36d5ec597087838372e4e277953ca
Johan296 <159972047+Johan296@users.noreply.github.com>
2025-05-29 12:19:52 +1200

    ichs

--- milestone_3/milestone_3_code.py
+++ milestone_3/milestone_3_code.py
@@ -4,46 +4,50 @@ score = 0
 print("Welcome to the Music Quiz!")
 
 # Question 1
-answer = input("What is the name of the artist who got featured in the song 'Like that'?")
-if answer== "Kendrick Lamar".lower():
+answer = input("What is the name of the artist who got featured in the song 'Like that'?").lower()
+if answer== "kendrick lamar":
     print("Correct!")
-elif answer== "Kendrick Lamar ".lower():
+elif answer== "kendrick lamar ":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 # Question 2
-answer = input("Who Found the XO record label?")
-if answer == "Weeknd".lower():
+answer = input("Who Found the XO record label?").lower()
+if answer == "weeknd":
     print("Correct!")
-elif answer== "Weekend".lower():
+elif answer== "weekend":
     print("Uhm Actually its Weeknd without the 'E', but you still got it right:)")
     score += 1
 else:
     print("Incorrect!")
 
 # Question 3
-answer = input("Which Artist released the album ' I AM MUSIC'?")
-if answer == "Playboi Carti".lower():
+answer = input("Which Artist released the album ' I AM MUSIC'?").lower()
+if answer == "playboi carti":
     print("Correct!")
-elif answer== "Weekend".lower():
+elif answer== "playboy cardi":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 # Question 4
-answer = input("Who sang the song 'Juno'?")
-if answer == "Sabrina carpenter".lower():
+answer = input("Which Rapper Changed his name to 'Ye'?").lower()
+if answer == "kanye West":
     print("Correct!")
-    score += 2
+elif answer== "kanye":
+    print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 # Question 5
-answer = input("What is the real name of rapper Eminem?")
-if answer == "Marshall Mathers".lower():
+answer = input("What is the real name of rapper Eminem?").lower()
+if answer == "marshall mathers":
+    print("Correct!")
+elif answer== "marshall mathers ":
     print("Correct!")
     score += 1
 else:
